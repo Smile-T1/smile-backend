45412aaa899d7859100a131b84639b5ac298cbc9
NadaAlfowey <101261526+NadaAlfowey@users.noreply.github.com>
2024-05-04 02:56:14 +0300

    feat: add get doctor/patient info

---
+++ routes/general.router.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import generalController from '../controllers/general.controller.js';
+import authCheck from '../middleware/auth/is-auth.js';
+import checkDentist from '../middleware/auth/is-dentist.js';
+
+const router = express.Router();
+
+router.get('/info', authCheck, generalController.getnfo);
+
+export default router;
