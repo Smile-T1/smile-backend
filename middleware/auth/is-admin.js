ebf5e050546711d5092dec18aa14c0539d3f8ba3
NadaAlfowey <101261526+NadaAlfowey@users.noreply.github.com>
2024-05-20 17:48:11 +0300

    feat: get all presecriptions

--- middleware/auth/is-admin.js
+++
@@ -1,9 +0,0 @@
-const adminCheck = (req, res, next) => {
-  if (req.roleaccess === 'Admin' && req.userId) {
-    next();
-  } else {
-    res.status(401).json({ message: 'Unauthorized!' });
-  }
-};
-
-export default adminCheck;
--- middleware/multer/uploadReport.js
+++
@@ -1,8 +0,0 @@
-import multer from 'multer';
-
-const storage = multer.memoryStorage();
-//single file upload(report)
-
-const uploadfile = multer({ storage: storage, limits: { fileSize: 1024 * 1024 * 5 } }).single('report');
-
-export default uploadfile;
